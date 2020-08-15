c4b3a78eeb64d00b6c70b0424562ae742a6c4218
zhupeng0521 <1512299897@qq.com>
2020-08-15 16:03:43 +0800

    初始化项目

---
+++ z-module-platform/src/main/java/com/module/zp/ZModulePlatformApplication.java
@@ -0,0 +1,16 @@
+package com.module.zp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhupeng
+ */
+@SpringBootApplication
+public class ZModulePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZModulePlatformApplication.class, args);
+    }
+
+}
